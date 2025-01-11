d18f144327e381d86a5ec090b1aaa0dace74c940
Miguel Lozano <mohara2099@byui.edu>
2025-01-11 12:57:44 -0700

    I think I manage to get the second part of the assignment done, for some reason it didn't want to run in the terminal but when I comared it to the sample solutions they are similar so I'm certain i'm on the right track

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?: ");
+        string answer = Console.ReadLine();
+        int grade_percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (grade_percent >= 90)
+        { letter = "B"; }
+
+        else if (grade_percent >= 80)
+        { letter = "B"; }
+
+        else if (grade_percent >= 70)
+        { letter = "C"; }
+
+        else if (grade_percent >= 60)
+        { letter = "D"; }
+
+        else
+        { letter = "F"; }
+
+        Console.WriteLine($"Your Grade is {letter}");
+
+        if (grade_percent>= 70)
+        {Console.WriteLine("You passed!");}
+        else
+        {Console.WriteLine("Well you can't win them all, study more and try again");}
     }
 }
