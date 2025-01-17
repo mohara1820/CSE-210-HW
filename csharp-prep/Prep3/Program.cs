68496e94aa6be240f5c59d553458a8e7d139428a
Miguel Lozano <mohara2099@byui.edu>
2025-01-17 14:39:05 -0700

    did the prep3 assignment and also uploaded the new sandbox file that we created in class

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int chosen_Number = randomGenerator.Next(1,101);
+
+        int guess_number = -1;
+
+        while (guess_number != chosen_Number)
+        {
+            Console.Write("what is your guess on the number? ");
+            guess_number = int.Parse(Console.ReadLine());
+
+            if (chosen_Number > guess_number)
+            {Console.WriteLine("Higher");}
+
+            else if (chosen_Number < guess_number)
+            {Console.WriteLine("Lower");}
+
+            else 
+            {Console.WriteLine("that is correct! good job!");}
+        }
     }
 }
